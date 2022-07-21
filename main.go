2b0a6a1283f5e938c893fa9fa1449ef8a053476a
Furkan ERYILMAZ <flatline.incommunicado@gmail.com>
2022-07-21 14:18:42 +0300

    bot initialization test

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	// "github.com/bwmarrin/discordgo"
-	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarin/discordgo"
 )
 
 func main() {
-	fmt.Println("Leblebi works")
+	var Session, _ = discordgo.New()
+	Session.Token = ""
+
+	err := Session.Open()
+	if err != nil {
+		log.Printf("Error establisihng connection to Discord, %s\n", err)
+		os.Exit(1)
+	}
+
+	log.Printf("Now running, press Ctrl + C to exit.")
+	sc := make(chan os.Signal, 1)
+
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Clean up
+	Session.Close()
 }
