7f6b1cd7df7251e62a0f551a5b85e933b20684a8
Furkan ERYILMAZ <flatline.incommunicado@gmail.com>
2022-07-24 18:31:29 +0300

    dummy command to test message sending

--- main.go
+++ main.go
@@ -1,33 +1,17 @@
 package main
 
-import (
-	"dpmmusicbot/stream"
-	"fmt"
-	"time"
-)
-
 func main() {
-	// discord, err := discordgo.New(utils.GoDotEnvVariable("DCTOKEN"))
+	ConnectToDiscord()
+
+	// start := time.Now()
+	// videoId, err := stream.YTSearchWithAPI("Fiyakalı Yok oluşlar")
+	// fmt.Printf("YT Search took %s\n", time.Since(start))
+	// opusUrl, err := stream.GetUrlFromYTWithId(videoId)
+	// fmt.Printf("YT Opus link extracting took %s\n\n", time.Since(start))
+
 	// if err != nil {
-	// 	fmt.Println("Error creating discord session,", err)
 	// 	return
 	// }
-	// err = discord.Open()
-	// if err != nil {
-	// 	fmt.Println("Error opening connection,", err)
-	// 	return
-	// }
-	// fmt.Println("Started")
-	// <-make(chan struct{})
-	start := time.Now()
-	videoId, err := stream.YTSearchWithAPI("Fiyakalı Yok oluşlar")
-	fmt.Printf("YT Search took %s\n", time.Since(start))
-	opusUrl, err := stream.GetUrlFromYTWithId(videoId)
-	fmt.Printf("YT Opus link extracting took %s\n\n", time.Since(start))
 
-	if err != nil {
-		return
-	}
-
-	fmt.Println(*opusUrl)
+	// fmt.Println(*opusUrl)
 }
